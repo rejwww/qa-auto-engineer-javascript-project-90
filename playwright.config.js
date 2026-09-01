f818f21dc990993f08a7b61e931b1e68b83c1f6c
Angelina Kuptsova <linakuptsova17@gmail.com>
2026-09-01 15:45:06 +0400

    update playwright config

--- playwright.config.js
+++ playwright.config.js
@@ -77,5 +77,11 @@ export default defineConfig({
   //   url: 'http://localhost:3000',
   //   reuseExistingServer: !process.env.CI,
   // },
+  webServer: {
+    command: 'npm run dev',       // команда, которая запускает твоё приложение
+    url: 'http://localhost:5173', // URL, который Playwright будет ждать
+    reuseExistingServer: !process.env.CI, // локально использует уже запущенный сервер
+    timeout: 60_000,              // сколько ждать старта (мс)
+  },
 });
 
